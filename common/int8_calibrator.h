083cbfce6673393d519e5fad59ae20b1912b94f7
caozhujia <caozhujia@proton.me>
2023-07-15 19:02:51 +0000

    add example image

--- common/int8_calibrator.h
+++ common/int8_calibrator.h
@@ -31,4 +31,7 @@ nvinfer1::IInt8Calibrator* get_int8_calibrator(const std::string& calibrator_typ
 class Int8EntropyCalibrator2 : public nvinfer1::IInt8EntropyCalibrator2
 {
 public:
-    Int8EntropyCalibrator2(int batch_size, const std::vector<std::vector<float>>& data
+    Int8EntropyCalibrator2(int batch_size, const std::vector<std::vector<float>>& data,
+            const std::string& calib_data_name = "", bool read_cache = true);
+        
+    ~Int8Entropy
