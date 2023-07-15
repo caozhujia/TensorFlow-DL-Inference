29f6119796f115f77a5014cded71b0a4dea2a1e4
caozhujia <caozhujia@proton.me>
2023-07-15 10:40:14 +0000

    ready for deployment

--- common/int8_calibrator.h
+++ common/int8_calibrator.h
@@ -3,4 +3,14 @@
  * @author      - wdn (dongnian.wang@outlook.com)
  * @brief       - tensorrt int8量化类声明
  * 
- * @copyright Copyright (c) 2021, wdn. All rights reser
+ * @copyright Copyright (c) 2021, wdn. All rights reserved.
+ * 
+ */
+#ifndef TRT_INT8_CALIBRATOR_H
+#define TRT_INT8_CALIBRATOR_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include 
