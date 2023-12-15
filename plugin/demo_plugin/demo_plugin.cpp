29c3d608e41e13b6ddd441b3abb5ff7a1309ce97
caozhujia <caozhujia@proton.me>
2023-12-15 05:55:24 +0000

    more pythonic

---
+++ plugin/demo_plugin/demo_plugin.cpp
@@ -0,0 +1,13 @@
+
+/**
+ * @file        - demo_plugin.cpp
+ * @author      - wdn (dongnian.wang@outlook.com)
+ * @brief 
+ * 
+ * @copyright Copyright (c) 2021, wdn. All rights reserved.
+ * 
+ */
+#include "demo_plugin.h"
+#include "helper.h"
+
+using namespace nvinfer1;
