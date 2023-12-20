640b8c9d7a964d136d208ad5118b295417dab883
caozhujia <caozhujia@proton.me>
2023-12-20 09:15:29 +0000

    fixing references

--- plugin/demo_plugin/demo_plugin.cpp
+++ plugin/demo_plugin/demo_plugin.cpp
@@ -107,3 +107,110 @@ nvinfer1::DataType DemoPlugin::getOutputDataType(int index,
 {
     log(" run here now! ");
     return nvinfer1::DataType::kFLOAT;
+}
+
+bool DemoPlugin::isOutputBroadcastAcrossBatch(int outputIndex, 
+    const bool* inputIsBroadcasted, int nbInputs) const
+{
+    log(" run here now! ");
+    return false;
+}
+
+bool DemoPlugin::canBroadcastInputAcrossBatch(int inputIndex) const
+{
+    log(" run here now! ");
+    return false;
+}
+
+void DemoPlugin::configurePlugin(const nvinfer1::Dims* inputDims, 
+    int nbInputs, const nvinfer1::Dims* outputDims, int nbOutputs, 
+    const nvinfer1::DataType* inputTypes, const nvinfer1::DataType* outputTypes,
+    const bool* inputIsBroadcast, const bool* outputIsBroadcast, 
+    nvinfer1::PluginFormat floatFormat, int maxBatchSize)
+{
+    log(" run here now! ");
+}
+
+void DemoPlugin::destroy()
+{
+    log(" run here now! ");
+}
+
+nvinfer1::IPluginV2Ext* DemoPlugin::clone() const
+{
+    log(" run here now! ");
+    auto* plugin = new DemoPlugin(m_layer_name);
+    plugin->setPluginNamespace(m_namespace.c_str());
+    return plugin;
+}
+
+void DemoPlugin::setPluginNamespace(const char* pluginNamespace)
+{
+    log(" run here now! ");
+}
+
+const char* DemoPlugin::getPluginNamespace() const
+{
+    log(" run here now! ");
+    return m_namespace.c_str();
+}
+
+/******DemoPluginCreator*******/
+DemoPluginCreator::DemoPluginCreator()
+{
+    log(" run here now! ");
+    m_fc.nbFields = 0;
+    m_fc.fields = nullptr;
+    m_plugin_name = "demo_plugin";
+    m_plugin_version = "01";
+}
+
+const char* DemoPluginCreator::getPluginName() const
+{
+    log(" run here now! ");
+    return m_plugin_name.c_str();
+}
+
+const char* DemoPluginCreator::getPluginVersion() const
+{
+    log(" run here now! ");
+    return m_plugin_version.c_str();
+}
+
+const nvinfer1::PluginFieldCollection* DemoPluginCreator::getFieldNames()
+{
+    log(" run here now! ");
+    return &m_fc;
+}
+
+nvinfer1::IPluginV2* DemoPluginCreator::createPlugin(const char* name, const nvinfer1::PluginFieldCollection* fc)
+{
+    log(" run here now! ");
+    auto* plugin = new DemoPlugin(name);
+    plugin->setPluginNamespace(m_namespace.c_str());
+    return plugin;
+}
+
+nvinfer1::IPluginV2* DemoPluginCreator::deserializePlugin(const char* name, 
+    const void* serialData, size_t serialLength)
+{
+    log(" run here now! ");
+    return new DemoPlugin(name, serialData, serialLength);
+}
+
+void DemoPluginCreator::setPluginNamespace(const char* pluginNamespace)
+{
+    log(" run here now! ");
+    m_namespace = pluginNamespace;
+}
+
+const char* DemoPluginCreator::getPluginNamespace() const
+{
+    log(" run here now! ");
+    return m_namespace.c_str();
+}
+
+/**
+ * 注册plugin ！！！！！！！！！！！！！！
+*/
+REGISTER_TENSORRT_PLUGIN(DemoPluginCreator);
---
+++ plugin/demo_plugin/demo_plugin.h
@@ -0,0 +1,8 @@
+
+/**
+ * @file        - demo_plugin.h
+ * @author      - wdn (dongnian.wang@outlook.com)
+ * @brief 
+ * 
+ * @copyright Copyright (c) 2021, wdn. All rights reserved.
+ * 
